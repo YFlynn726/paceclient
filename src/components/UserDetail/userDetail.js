9aeb4829d42f2bf2272ad780c50cc49334c77baa
Yajaira Flynn-Corbin <yary.ba26@gmail.com>
2020-06-11 12:24:46 -0400

    clean code

--- src/components/UserDetail/userDetail.js
+++ src/components/UserDetail/userDetail.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import PaceContext from "../PaceContext";
-import ValidateError from "../ValidateError";
 import { Modal } from "antd";
 import moment from "moment";
 import config from "../../config";
